1ad1783d6538fa55a64b3de388688fbd332dfe64
raccoonjeong <rana3782@gmail.com>
2023-01-09 18:57:28 +0900

    Section03 전략 패턴과 콜백 패턴

---
+++ src/main/java/hello/advanced/app/v5/OrderServiceV5.java
@@ -0,0 +1,37 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.callback.TraceCallback;
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceV5 {
+    private final OrderRepositoryV5 orderRepository;
+    private final TraceTemplate template;
+
+    public OrderServiceV5(OrderRepositoryV5 orderRepository, LogTrace trace) {
+        this.orderRepository = orderRepository;
+        this.template = new TraceTemplate(trace);
+    }
+    public void orderItem(String itemId) {
+        template.execute("OrderService.orderItem()",
+                () -> {
+                    orderRepository.save(itemId);
+                    return null;
+                });
+
+//        AbstractTemplate<Void> template = new AbstractTemplate(trace) {
+//            @Override
+//            protected Void call() {
+//                orderRepository.save(itemId);
+//                return null;
+//            }
+//        };
+//
+//        template.execute("OrderService.orderItem()");
+
+    }
+}
